22b4a8772b105510f368e79066cbf401625f0807
Rajendra <itsrajendrapatel@gmail.com>
2023-03-05 21:08:12 +0530

    Nabar Added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Footer from "../../Components/Footer";
+import Navbar from "../../Components/Navbar";
 
 export default function Home() {
   return (
@@ -9,7 +11,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello world </h1>
+      <Navbar />
+      <Footer />
     </>
   );
 }
