2e9fef27612b1873755d1dc292ccf392a97747ac
Rajendra <itsrajendrapatel@gmail.com>
2023-03-07 21:20:33 +0530

    cart state management

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,17 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Footer from "../../Components/Footer";
 import Navbar from "../../Components/Navbar";
 import "@/styles/globals.css";
+import { Provider } from "react-redux";
+import { store } from "../../Redux/store";
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
+      <Provider store={store}>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </Provider>
     </ChakraProvider>
   );
 }
