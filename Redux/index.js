c30afecabbc34c1de80eb3c4db8bcacba4bbb498
Rajendra <itsrajendrapatel@gmail.com>
2023-03-09 14:04:07 +0530

    Redux Toolkit Started

--- Redux/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import cartReducer from "./cartRedux/reducer";
-export const rootReducer = combineReducers({
-  cartReducer,
-});
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,17 +2,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Footer from "../../Components/Footer";
 import Navbar from "../../Components/Navbar";
 import "@/styles/globals.css";
-import { Provider } from "react-redux";
-import { store } from "../../Redux/store";
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Provider store={store}>
-        <Navbar />
-        <Component {...pageProps} />
-        <Footer />
-      </Provider>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
     </ChakraProvider>
   );
 }
