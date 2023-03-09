095e7d1a219482cb8e9f45f66f6e3717f12db854
Rajendra <itsrajendrapatel@gmail.com>
2023-03-09 23:45:24 +0530

    Login Page Done

--- src/pages/SmallPages/Login.js
+++ src/pages/SmallPages/Login.js
@@ -1,11 +1,75 @@
-import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import Image from "next/image";
 
-// import { useToast } from "@chakra-ui/react";
-// import Image from "next/image";
-// import style from "../../styles/Login.module.css";
-
-const Login = () => {
-  return <>Login</>;
-};
-
-export default Login;
+export default function Login() {
+  return (
+    <Flex
+      backgroundColor={"blue.200"}
+      p="5rem"
+      justifyContent={"center"}
+      alignContent="center"
+      gap={"10rem"}
+      boxShadow="dark-lg"
+    >
+      <Image src="/login.png" alt={`Login Img`} width={500} height={100} />
+      <Box
+        backgroundColor={"white"}
+        width="30%"
+        borderRadius={"1rem"}
+        padding="10px"
+        display={"flex"}
+        flexDir="column"
+        justifyContent={"center"}
+        alignContent="center"
+      >
+        <Heading textAlign={"center"}>Login</Heading>
+        <Flex
+          border={"1px solid"}
+          justifyContent="center"
+          m="1rem"
+          borderRadius={"10rem"}
+          width="10%"
+          margin={"auto"}
+          mt="1rem"
+          mb="1rem"
+        >
+          <Image
+            src="/profile.png"
+            alt={`profile Img`}
+            width={200}
+            height={100}
+          />
+        </Flex>
+        <FormLabel fontWeight={"normal"}>First name</FormLabel>
+        <Input id="first-name" backgroundColor="blue.200" />
+        <FormLabel fontWeight={"normal"}>First name</FormLabel>
+        <Input id="first-name" backgroundColor="blue.200" />
+        <Button
+          width={"100%"}
+          borderRadius="1rem"
+          backgroundColor={"blue.400"}
+          color="white"
+          mt={"1rem"}
+        >
+          Submit
+        </Button>
+        <Text textAlign={"center"} mt="10px">
+          Forget Password?{" "}
+          <span style={{ color: "blue" }}>Get it on Email !</span>
+        </Text>
+      </Box>
+    </Flex>
+  );
+}
